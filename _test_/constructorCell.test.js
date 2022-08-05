caf0cc8265b8f935bdb6782b0e23f485556b6cab
joseocampob <109179412+joseocampob@users.noreply.github.com>
2022-08-05 18:17:02 -0500

    Added Cell Tests
    Implemented test for CellConstructor and FindNeighbour

---
+++ _test_/constructorCell.test.js
@@ -0,0 +1,23 @@
+const { Cell } = require('../Cell')
+const { Board } = require('../Board')
+
+test('Cell position is a number', () => {
+    let test = new Cell(0, 1)
+    expect((isNaN(test.x))).toBe(false)
+    expect((isNaN(test.y))).toBe(false)
+})
+
+test('Quantity of cell neighbours are recieved correctly', () => {
+    let test = new Cell(0, 4)
+    let tablero = [
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 1, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+      ];
+
+    expect((test.getNeighbour(tablero, 4, 8))).toBe(1)
+})
+
+//npm init -y
+//npm i jest --save-dev
